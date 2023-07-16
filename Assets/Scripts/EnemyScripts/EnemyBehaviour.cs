6835b2d255c37ad903645286cd7992cacf72c5f1
Abdulrhman Ahmed <abd671998@gmail.com>
2023-07-16 15:46:01 +0300

    Adding animation logic to movement and combat

--- Assets/Scripts/EnemyScripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyScripts/EnemyBehaviour.cs
@@ -4,23 +4,77 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    [SerializeField]int enemyHealth;
+    public int enemyHealth;
+    [SerializeField]float attackThreshold;
+    [SerializeField] float followThreshold;
+    float distance;
+    [SerializeField] Transform player;
+    [SerializeField] Transform startPostion;
+    [SerializeField] private float offset;
+    Rigidbody2D rb;
+    Animator anim;
+    [SerializeField] private float enimySpped;
+    public bool isAttacking = false;
+    Vector3 direction;
+    [SerializeField] LayerMask playerMask;
+    [SerializeField] Transform effectPoint;
+    [SerializeField] float effectRadius;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        
+        distance = Vector2.Distance(transform.position, player.position);
+        
+        if (distance <= followThreshold)
+        {
+            Debug.Log("kshaa");
+            
+
+            //if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Monster_Attack_animation"))
+            //{
+                
+            // }
+
+            if (distance <= attackThreshold)
+            {
+                anim.SetBool("Attack", true);
+                anim.SetBool("Walk", false);
+                Invoke(nameof(attackAnimation),2f);
+            }
+            else
+            {
+                
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.position.x + offset, transform.position.y), Time.deltaTime * enimySpped);
+                anim.SetBool("Attack", false);
+                anim.SetBool("Walk", true);
+            }
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+            anim.SetBool("Walk", false);
+        }
+        
+
+
+
+
+
+
     }
 
     public void TakeDamage(int damage) {
 
         if (enemyHealth <= 0)
         {
+            Debug.Log("Enemy Die");
             Die();
         }
         else { enemyHealth -= damage; }
@@ -31,6 +85,18 @@ public class EnemyBehaviour : MonoBehaviour
 
     public void Die()
     {
-        this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        Debug.Log("Enemy Die 2");
+        Destroy(gameObject);
+       // this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
     }
+
+    public void attackAnimation()
+    {
+       
+       anim.SetBool("Attack",false);
+        CancelInvoke();
+      
+    }
+
+    
 }
--- Assets/Scripts/PlayerScripts/PlayerCombatSystem.cs
+++ Assets/Scripts/PlayerScripts/PlayerCombatSystem.cs
@@ -10,19 +10,24 @@ public class PlayerCombatSystem : MonoBehaviour
     public LayerMask enemyLayer;
     public int attackRange;
     [SerializeField]int attackDamage = 10;
+    [SerializeField]int parryDamage = 10;
     PlayerMovement playermovement;
+    GameObject Enemy;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         playerHealth = gameObject.GetComponent<PlayerBehaviour>().health;
         playermovement = gameObject.GetComponent<PlayerMovement>();
         anim = gameObject.GetComponent<Animator>();
+        Enemy = GameObject.FindGameObjectWithTag("Enemy");
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+      
         if(Input.GetKey(KeyCode.E) && !playermovement.isAttacking){
 
             Attack();
@@ -41,21 +46,30 @@ public class PlayerCombatSystem : MonoBehaviour
         if (playerHealth <= 200)
         {
             GameManger.currentGameMode = GameManger.gameMode.normal;
+            
         }
         //anim.SetTrigger("attack");
 
         Collider2D[] hitPoints = Physics2D.OverlapCircleAll(effectPoint.position, attackRange, enemyLayer);
+        
         foreach (Collider2D enemy in hitPoints)
         {
+            
             Debug.Log("hit : "+ enemy.name);
             if (GameManger.currentGameMode == GameManger.gameMode.reverse)
             {
+                if (enemy.GetComponent<EnemyBehaviour>().enemyHealth < 500)
+                {
+                    parryDamage = 500;
+                }
                 playerHealth -= attackDamage;
-                Debug.Log("Self damage");
+                Debug.Log("Self damage player health : "+playerHealth);
             }
             else if (GameManger.currentGameMode == GameManger.gameMode.normal)
             {
+                parryDamage = 0;
                 enemy.GetComponent<EnemyBehaviour>().TakeDamage(attackDamage);
+                attackDamage = 500;
                 Debug.Log("Enemy damage");
             }
             
@@ -64,8 +78,17 @@ public class PlayerCombatSystem : MonoBehaviour
     }
 
     void Parry() {
+        if (Enemy.GetComponent<EnemyBehaviour>().isAttacking)
+        {
+            Enemy.GetComponent<EnemyBehaviour>().TakeDamage(parryDamage);
 
-        anim.SetTrigger("Parry");
+            anim.SetTrigger("Parry");
+        }
+        else
+        {
+            anim.SetTrigger("Parry");
+        }
+        
 
     }
 
